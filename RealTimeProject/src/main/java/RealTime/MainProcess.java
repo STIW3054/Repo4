f96f40f28d53ca97efcc73c6828d47d53b9db48d
Aman748 <984184589@qq.com>
2017-12-22 03:56:37 +0800

    MainProcess

---
+++ RealTimeProject/src/main/java/RealTime/MainProcess.java
@@ -0,0 +1,9 @@
+
+package RealTime;
+
+//for main process
+public class MainProcess {
+    public static void main(String[] args) throws InterruptedException {
+        CloneRepository.CloneRep();
+    }
+}
