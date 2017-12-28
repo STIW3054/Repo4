8d1f7b092c5055007a0a0a7918ab9ce692634ac5
Cheryn Lee <31918267+LeeCheryn@users.noreply.github.com>
2017-12-28 23:30:33 +0800

    Delete OutputFile.java

--- RealTimeProject/src/main/java/RealTime/OutputFile.java
+++
@@ -1,7 +0,0 @@
-package RealTime;
-
-//create output file and store list of files that can be successfully run (parse matricNo and java files name)
-//store to Output folder
-public class OutputFile {
-    
-}
