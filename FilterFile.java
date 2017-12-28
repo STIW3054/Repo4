a4ad15b65c831783da3f1e0962755e5d4cd50b41
NUR FARHANA FARISHAMUDIN <nurf4rh4n4@gmail.com>
2017-12-28 22:42:07 +0800

    Update FilterFile.java

--- FilterFile.java
+++ FilterFile.java
@@ -1,25 +1,23 @@
 package RealTime;
 
 import java.io.File;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  *
- * @author
+ * @author Group4
  */
+
 public class FilterFile {
 
     public static List <File> filter_files_by_type (String Filetype,File Path, List <File> FileList){
-        //determined weather the file exist
+        //determine weathe the file exist
             if (!Path.exists()){
-                System.out.println("The path " + Path + " not existing!");
+//                System.out.println("The path " + Path + " not existing!");
             }else if (Path.isFile()){
                     //If the current fir is a file, then determine what file
-                    String firName = Path.getName();//获取文件名
-                    //System.err.println(firName.hashCode());  获取文件后缀名
+                    String firName = Path.getName();//get file name
+                    //System.err.println(firName.hashCode());  //get file extension
                     if(firName.substring(firName.lastIndexOf(".")+1) .equals(Filetype)){
                         FileList.add(Path);
                     }
@@ -35,12 +33,12 @@ public class FilterFile {
 
     public static List <File> filter_files_by_name (String FileName,File Path,List <File> FileList){
         if (!Path.exists()){
-                System.out.println("The path " + Path + " not existing!");
+//                System.out.println("The path " + Path + " not existing!");
         }else if(Path.isFile()){
             if(Path.getName().lastIndexOf(".")!=0 && Path.getName().lastIndexOf(".")!=(-1)){
                 String filename = Path.getName();
                 if(filename.substring(0,filename.lastIndexOf(".")).equals(FileName)&&!filename.substring(filename.lastIndexOf(".")+1).equals("java")){
-                    System.out.println(Path.toPath());
+//                    System.out.println(Path.toPath());
                     FileList.add(Path);
                 }
             }
@@ -69,45 +67,4 @@ public class FilterFile {
         }
         return r;
     }
-
-    public static void main(String[] args) {
-        List <File> FileList = new ArrayList();
-        File Path = new File("/Users/Aman/Desktop/Repos/240448");
-        FileList = FilterFile.filter_files_by_type("java", Path, FileList);
-        System.out.println(FileList.toString());
-    }
-//    public static List <File> getMain(List <File> FileList){
-//
-//        File [] FileArray = new File [FileList.size()];
-//        FileList.toArray(FileArray);
-//        List <File> MainFile = new <File> ArrayList();
-//        for (File file : FileArray){
-//            String fileName = file.getName();
-//            int removeType = fileName.lastIndexOf(".");
-//            if (removeType > 0) {
-//                fileName = fileName.substring(0, removeType);
-//            }
-//            CompileFile f = new CompileFile();
-//            String packageName = file.getParentFile().getName();
-//
-//            Method mainMethod;
-//            try{
-//                Class name;
-//                name = Class.forName(fileName);
-//                if(!packageName.equals("")){
-//                    packageName = packageName.replace("/",".");
-//                    name = Class.forName(packageName+"."+fileName);
-//                }else {
-//                    name = Class.forName(fileName);
-//                }
-//                mainMethod = name.getMethod("main", new Class[]{String[].class});
-//                if(mainMethod != null){
-//                    MainFile.add(file);
-//                }
-//            }catch(ClassNotFoundException | NoSuchMethodException | SecurityException e){
-//                System.out.println(e);
-//            }
-//        }
-//        return MainFile;
-//    }
 }
